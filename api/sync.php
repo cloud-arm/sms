8f828040a90e5260099e1aaf07142011adf95485
ashen <hansakagaa@gmail.com>
2024-04-23 06:34:11 +0530

    up

--- api/sync.php
+++ api/sync.php
@@ -1,5 +1,5 @@
 <?php
-include('../../connect.php');
+include('../connect.php');
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
